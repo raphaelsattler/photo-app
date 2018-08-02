2d19a09940ff226c444d67c973a6c8bcb0f25496
raphaelsattler <raphaelsattler@hotmail.com>
2018-08-02 14:59:04 -0300

    Remove storage :file

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,10 +6,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
+  storage :fog
+  #storage :file
   end
 
   # Override the directory where uploaded files will be stored.
